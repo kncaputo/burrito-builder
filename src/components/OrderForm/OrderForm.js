9e0ded9cff1110da4a3cf217586cb49ca6e67a8c
Kara Caputo <kncaputo@gmail.com>
2021-01-19 13:52:56 -0500

    Swap out error alert for error render on display

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -6,21 +6,24 @@ class OrderForm extends Component {
     this.props = props;
     this.state = {
       name: '',
-      ingredients: []
+      ingredients: [],
+      error: ''
     };
   }
 
   handleNameChange = e => {
-    this.setState({ name: e.target.value})
+    this.setState({ error: '' });
+    this.setState({ name: e.target.value});
   }
 
   handleIngredientChange = e => {
     e.preventDefault();
+    this.setState({ error: '' });
 
     if (this.state.ingredients.length > 0) {
-      this.setState( { ingredients: [e.target.name, ...this.state.ingredients] })
+      this.setState( { ingredients: [e.target.name, ...this.state.ingredients] });
     } else {
-      this.setState( { ingredients: [e.target.name] })
+      this.setState( { ingredients: [e.target.name] });
     }
   }
 
@@ -28,12 +31,12 @@ class OrderForm extends Component {
     e.preventDefault();
 
     if (this.state.name.length <= 1) {
-      alert('Please include a name on your order');
+      this.setState({ error: 'Please provide a name' });
     } else if (this.state.ingredients.length === 0) {
-      alert('Please select at least one ingredient')
+      this.setState({ error: 'Please select at least one ingredient' });
     } else {
       const id = Date.now();
-      const order = {id, ...this.state};
+      const order = {id, name: this.state.name, ingredients: this.state.ingredients };
   
       this.props.createOrder(order);
       this.clearInputs();
@@ -68,6 +71,9 @@ class OrderForm extends Component {
 
         <p>Order: { this.state.ingredients.join(', ') || 'Nothing selected' }</p>
 
+        {this.state.error &&
+          <p>Please provide a name and select at least one ingredient</p>
+        }
 
         <button onClick={e => this.handleSubmit(e)}>
           Submit Order
